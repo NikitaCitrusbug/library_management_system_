67b6cbb6e56f0c37388848ae727b081e1b7e28cc
Nikita <nikita1.citrusbug@gmail.com>
2022-05-16 18:19:48 +0530

    CRUD opeartions completed

---
+++ librarymanagement/librarysystem/forms.py
@@ -0,0 +1,82 @@
+from dataclasses import fields
+from tkinter import Widget
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from .models import Author, Category, User , Book
+class CustomUserCreationForm(UserCreationForm):
+    class Meta(UserCreationForm.Meta):
+        model = User
+        fields = ['username', 'first_name', 'last_name','email']
+
+    def save(self,commit=True):
+        user = super().save(commit=False)
+        user.is_librarian = True
+        if commit:
+            user.save()
+        return user
+
+
+class CustomMemberCreationForm(UserCreationForm):
+    class Meta(UserCreationForm.Meta):
+        model = User
+        fields = ['username', 'first_name', 'last_name','email']
+
+    def save(self,commit=True):
+        user = super().save(commit=False)
+        user.is_member = True
+        if commit:
+            user.save()
+        return user
+
+
+
+class LoginForm(UserCreationForm):
+    class Meta(UserCreationForm.Meta):
+        model = User
+        fields = ['username' , 'password']
+
+
+
+class AddForm(forms.ModelForm):
+    class Meta:
+        model = Book
+        fields = '__all__'
+                
+        widgets = {
+            'name':forms.TextInput(attrs={'class':'form-control','placeholder':'Name'}),
+            'discription':forms.TextInput(attrs={'class':'form-control','placeholder':'Discription'}),
+            'quantity':forms.NumberInput(attrs={'class':'form-control','placeholder':'quantity'}),
+            'category':forms.Select(attrs={'class':'form-control form-select','placeholder':'select category'}),
+           
+        }
+
+     
+
+class UpdateBookForm(forms.ModelForm):
+    class Meta:
+        model = Book
+        fields = '__all__'
+
+class AddCategoryForm(forms.ModelForm):
+    class Meta:
+        model = Category
+        fields = '__all__'
+
+
+class UpdateCategoryForm(forms.ModelForm):
+    class Meta:
+        model = Category
+        fields = '__all__'
+
+class AddAuthorForm(forms.ModelForm):
+    class Meta:
+        model = Author
+        fields = '__all__'
+
+class UpdateAuthorForm(forms.ModelForm):
+    class Meta:
+        model = Author
+        fields = '__all__'
+
+
+
